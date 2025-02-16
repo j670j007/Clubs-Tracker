a40f5aafdc54bd84f697f165ae161a9560b2f740
unknown <anilthapa@ku.edu>
2025-02-16 11:35:11 -0600

    Comment on current landing

--- frontend/src/components/Landing/Landing.jsx
+++ frontend/src/components/Landing/Landing.jsx
@@ -1,8 +1,27 @@
+/*
+File: Landing.jsx
+Description: Landing page component
+Author(s): Anil Thapa
+Creation Date: 02/15/2025
+
+Preconditions:
+- Vite application running
+
+Input Values:
+- N/A
+
+Return Values:
+- Plain text for WIP
+
+Error Conditions:
+- N/A
+*/
+
 import { useNavigate } from 'react-router-dom';
 
 function Landing () {
-    const navigate = useNavigate();
-
+    const navigate = useNavigate(); // for future use 
+    // right now this is just a WIP, empty (for sprint 2)
     return(
     <div>
         <p>Sprint 2 Development</p>
