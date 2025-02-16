2fac7bba94c25502f9129a96894ea690ad01d7ed
unknown <anilthapa@ku.edu>
2025-02-16 11:27:39 -0600

    comment app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,25 @@
-import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
+/*
+File: App.jsx
+Description: Frontend Application for Club Manager
+Author(s): Anil Thapa
+Creation Date: 02/15/2025
+
+Preconditions:
+- Dependencies in package-lock.json and package.json installed
+- Node installed
+
+Input Values:
+- User inputs through interactions
+
+Return Values:
+- Feedbacks the user based on what they expect to see 
+
+Error Conditions:
+- Intentionally messing with routing
+*/
+
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'; // Routing package
+import { AuthProvider } from './context/AuthContext'; // Our Authentication context used to maintain a session while browsing
 import Login from './components/Login/Login';
 import Register from './components/Register/Register'
 import Landing from './components/Landing/Landing'
@@ -7,13 +27,13 @@ import './App.css'
 
 function App() {
   return (
-    <Router>
-      <AuthProvider>
-        <Routes>
+    <Router> {/* Router to wrap around */}
+      <AuthProvider> {/* Authentication context for the components to inherit auth needs */}
+        <Routes> {/* Routes setup */}
           <Route path="/login" element={<Login/>}/>
           <Route path="/register" element={<Register/>}/>
           <Route path="/dashboard" element={<Landing/>}/>
-          <Route path="*" element={<Navigate to ="/login" replace/>}/> /* this will be changed with some safety later */
+          <Route path="*" element={<Navigate to ="/login" replace/>}/> {/* this will be changed with some safety later */}
         </Routes>
       </AuthProvider>
     </Router>
