d6c0a296269f5d0d7d620ea8335138aa2f8f5136
unknown <anilthapa@ku.edu>
2025-02-15 23:57:32 -0600

    create filler for landing and spr2 start

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register'
+import Landing from './components/Landing/Landing'
 import './App.css'
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login/>} />
-        <Route path="/register" element={<Register/>} />
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/dashboard" element={<Landing/>}/>
       </Routes>
     </Router>
   )
--- frontend/src/components/Landing/Landing.jsx
+++ frontend/src/components/Landing/Landing.jsx
@@ -1,7 +1,13 @@
+import { useNavigate } from 'react-router-dom';
+
 function Landing () {
+    const navigate = useNavigate();
+
+    return(
     <div>
-        <p>Landing</p>
+        <p>Sprint 2 Development</p>
     </div>
+    )
 }
 
 export default Landing
