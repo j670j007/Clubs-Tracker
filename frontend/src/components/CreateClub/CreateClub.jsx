3e7e67e3b082380fd7c2b9256d32c3f114c4cfb5
unknown <anilthapa@ku.edu>
2025-03-02 12:18:32 -0600

    start club create module

---
+++ frontend/src/components/CreateClub/CreateClub.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import './CreateClub.css';
+
+function CreateClub() {
+    const [formData, setFormData] = useState({
+        name: '',
+        description: '',
+        code: ''
+      });
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default CreateClub
