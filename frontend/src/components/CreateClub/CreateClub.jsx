dfc90a0493df6cf75e89cc15615cca62106201a9
unknown <anilthapa@ku.edu>
2025-03-02 12:33:33 -0600

    create submission handling for create club

--- frontend/src/components/CreateClub/CreateClub.jsx
+++ frontend/src/components/CreateClub/CreateClub.jsx
@@ -6,11 +6,44 @@ function CreateClub() {
         name: '',
         description: '',
         code: ''
-      });
+    });
+
+    const [error, setError] = useState('');
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setError('');
+
+        try {
+            const response = await fetch('http://127.0.0.1:5000/create_club', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `${localStorage.getItem('token')}`
+                },
+                body: JSON.stringify({
+                    club_name: formData.name,
+                    club_desc: formData.description,
+                    invite_code: formData.code
+                }),
+            });
+            onSuccess(console.log(response.message));
+            return true;
+        } catch (error) {
+            setError(error);
+            return false;
+        }
+    };
 
     return (
         <div>
-            
         </div>
     );
 }
